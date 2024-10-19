ee107d8fb97aaecaea7171d3b3348554b3a91c29
wtto00 <wtto00@outlook.com>
2024-10-20 07:58:57 +0800

    fix: more rigorous check.

--- src/index.ts
+++ src/index.ts
@@ -72,7 +72,7 @@ export function presetTheme<T extends Record<string, any>>(options: PresetThemeO
                       if (cssColor?.alpha !== undefined && cssColor?.alpha !== null) {
                         if (`var(${cssVarName}--alpha, 1)` === cssColor.alpha) {
                           const values = themeValues.get(name)
-                          if (values)
+                          if (values?.theme[themeKey][`${name}--alpha`])
                             themeAlphaValue = values.theme[themeKey][`${name}--alpha`]
                         }
                         else {
@@ -82,7 +82,7 @@ export function presetTheme<T extends Record<string, any>>(options: PresetThemeO
                       if (cssColor?.components) {
                         if (cssColor.components.length === 1 && `var(${cssVarName})` === cssColor.components[0]) {
                           const values = themeValues.get(name)
-                          if (values)
+                          if (values?.theme[themeKey][name])
                             themeValue = values.theme[themeKey][name]
                         }
                         else {
@@ -93,7 +93,7 @@ export function presetTheme<T extends Record<string, any>>(options: PresetThemeO
                     else {
                       if (`var(${cssVarName})` === themeValue) {
                         const values = themeValues.get(name)
-                        if (values) {
+                        if (values?.theme[themeKey][name]) {
                           themeValue = values.theme[themeKey][name]
                         }
                       }
